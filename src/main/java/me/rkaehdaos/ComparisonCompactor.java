a9c36c7255b63d9daec2ad5a48456fd90fa322d5
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-11 14:28:08 +0900

    [#2 Junit4 Refactoring] 함수정리

--- src/main/java/me/rkaehdaos/ComparisonCompactor.java
+++ src/main/java/me/rkaehdaos/ComparisonCompactor.java
@@ -55,14 +55,21 @@ public class ComparisonCompactor {
 
     private void findCommonPrefixAndSuffix() {
         findCommonPrefix();
-        int expectedSuffix = expected.length() - 1;
-        int actualSuffix = actual.length() - 1;
-        for (; actualSuffix >= prefixIndex && expectedSuffix >= prefixIndex; actualSuffix--, expectedSuffix--) {
-            if (expected.charAt(expectedSuffix) != actual.charAt(actualSuffix)) {
+        int suffixLength = 1;
+        for (; !(suffixOverlapsPrefix(suffixLength)); suffixLength++) {
+            if (charFromEnd(expected, suffixLength) != charFromEnd(actual, suffixLength)) {
                 break;
             }
         }
-        suffixIndex = expected.length() - expectedSuffix;
+        suffixIndex = suffixLength;
+    }
+
+    private char charFromEnd(String s, int i) {
+        return s.charAt(s.length() - i);
+    }
+
+    private boolean suffixOverlapsPrefix(int suffixLength) {
+        return actual.length() - suffixLength < prefixIndex || expected.length() - suffixLength < prefixIndex;
     }
 
     private void findCommonPrefix() {
