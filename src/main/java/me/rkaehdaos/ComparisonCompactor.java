ef72a57cbb52720426c01ffbaaf76e0f748b93f7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-11 12:05:18 +0900

    [#2 Junit4 Refactoring] 조건문 캡슐화
    - 의도를 명확히 표현하려면 조건문을 캡슐화해야 한다
    - 조건을 메서드로 뽑아내 적절한 네이밍

--- src/main/java/me/rkaehdaos/ComparisonCompactor.java
+++ src/main/java/me/rkaehdaos/ComparisonCompactor.java
@@ -22,7 +22,7 @@ public class ComparisonCompactor {
 
     @SuppressWarnings("deprecation")
     public String compact(String message) {
-        if (expected == null || actual == null || areStringsEqual()) {
+        if (shouldNotCompact()) {
             return Assert.format(message, expected, actual);
         }
 
@@ -31,6 +31,10 @@ public class ComparisonCompactor {
         String expected = compactString(this.expected);
         String actual = compactString(this.actual);
         return Assert.format(message, expected, actual);
+    }
+
+    private boolean shouldNotCompact() {
+        return expected == null || actual == null || areStringsEqual();
     }
 
     private String compactString(String source) {
